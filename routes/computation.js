6298987a33cf819d71ff66557347af1851d46dc8
srinivas3p <87357998+srinivas3p@users.noreply.github.com>
2023-10-25 21:32:25 -0500

    hgvdhgv

--- routes/computation.js
+++ routes/computation.js
@@ -1,22 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET users listing. */
+router.get('/', function (req, res, next) {
     fake_url = "https://fake.com/path" + req.url
     const url = new URL(fake_url)
     const search_params = url.searchParams
-    if(req.method == 'GET'){
+    if (req.method === 'GET') {
         value = search_params.get("x")
-        if(value == null)
+        if(value === null)
         {
-            value=Math.random(Math.random()*100);
-        }
-        res.writeHead(200,{ 'Content-Type': 'text/html'});
-        res.write('Math.hypot() applied to '+ val + 'is' +Math.hypot(2,value));
-        res.write('Math.hypot() applied to '+ val + 'is' +Math.ceil(value));
-        res.write('Math.hypot() applied to '+ val + 'is' +Math.clz32(value));
+            value=Math.round(Math.random()*800);
+        }     
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('Math.hypot() applied to ' + value + ' is ' + Math.hypot(2,value));
+        res.write('<br>Math.ceil() applied to ' + value + ' is ' + Math.ceil(value))
+        res.write('<br>Math.clz32() applied to ' + value + ' is ' + Math.clz32(value))
         res.end()
     }
 
